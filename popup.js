2e812065534616422d01f0d6762c593a653260fb
Huzaifa Elahi <huzaifaelahi1@yahoo.com>
2018-09-09 17:28:03 -0400

    Managed Alert functionality using JS

---
+++ popup.js
@@ -0,0 +1,21 @@
+function newFunction() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    
+    alert(text);  
+}
+
+chrome.browserAction.onClicked.addListener(function(){
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    
+    alert(text);    });
+
