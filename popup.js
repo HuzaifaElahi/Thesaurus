2079e2397d5e9f0ff0444ba6e9ac80718062f15f
Huzaifa Elahi <huzaifaelahi1@yahoo.com>
2018-09-09 19:20:54 -0400

    Added JQuery

--- popup.js
+++ popup.js
@@ -1,14 +1,3 @@
-function newFunction() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    
-    alert(text);  
-}
-
 chrome.browserAction.onClicked.addListener(function(){
     var text = "";
     if (window.getSelection) {
@@ -16,6 +5,40 @@ chrome.browserAction.onClicked.addListener(function(){
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
     }
-    
-    alert(text);    });
+    var request = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-ru&text=egg"+text;
 
+    tt = request.concat(text);
+
+    alert(range);   
+ });
+
+ document.addEventListener('DOMContentLoaded', function () {
+
+    window.onload=document.getElementById("button").addEventListener("click",function GetSelectedText () {
+        if (window.getSelection) {  // all browsers, except IE before version 9
+            var range = window.getSelection ();
+
+            var url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=";
+            var word = egg;
+            var total = url + range;
+            $.getJSON( total, function( data ) {
+               
+              });
+              ;
+
+            alert (range.toString ());
+     } 
+     else {
+         if (document.selection.createRange) { // Internet Explorer
+                var range = document.selection.createRange ();
+
+                var url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=";
+                var word = range;
+                loadJSON(url+range);
+                print(data);
+
+                alert (range.text);
+            }
+     }
+    });
+});
