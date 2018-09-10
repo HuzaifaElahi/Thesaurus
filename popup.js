4ca6b7beff516b2c8813fed12f3588c9a6990533
Huzaifa Elahi <huzaifaelahi1@yahoo.com>
2018-09-09 20:50:11 -0400

    Working GET request; popup highlighting functional

--- popup.js
+++ popup.js
@@ -13,32 +13,20 @@ chrome.browserAction.onClicked.addListener(function(){
  });
 
  document.addEventListener('DOMContentLoaded', function () {
-
     window.onload=document.getElementById("button").addEventListener("click",function GetSelectedText () {
-        if (window.getSelection) {  // all browsers, except IE before version 9
+        if (window.getSelection) {  
             var range = window.getSelection ();
 
             var url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=";
-            var word = egg;
-            var total = url + range;
-            $.getJSON( total, function( data ) {
-               
-              });
-              ;
+            var word = "laid";
+            var total = url + word;
+            $.getJSON(total, function( data ) {
+                x=data;
+                print(x);
+                alert (x);
+            });
 
             alert (range.toString ());
      } 
-     else {
-         if (document.selection.createRange) { // Internet Explorer
-                var range = document.selection.createRange ();
-
-                var url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=";
-                var word = range;
-                loadJSON(url+range);
-                print(data);
-
-                alert (range.text);
-            }
-     }
     });
 });
