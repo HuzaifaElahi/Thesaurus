7b77537ca0a28a18f3f0d0919f55a56001488e92
Huzaifa Elahi <huzaifaelahi1@yahoo.com>
2018-09-10 00:19:47 -0400

    Form validates info and API call functionality is functional

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Dictionary-
+# Thesaurus-
+Type in a word to see numerous synonyms
+Availiable in the Chrome store!
+Additional functionality coming soon! 
--- popup.js
+++ popup.js
@@ -1,38 +1,20 @@
-chrome.browserAction.onClicked.addListener(function(){
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    var request = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-ru&text=egg"+text;
-
-    tt = request.concat(text);
-
-    alert(range);   
- });
-
  document.addEventListener('DOMContentLoaded', function () {
     window.onload=document.getElementById("button").addEventListener("click",function GetSelectedText () {
-        if (window.getSelection) {  
-            var range = window.getSelection ();
 
-            var url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=book";
-           // var word = "book";
-           // var total = url + word;
-            $.getJSON(url, function( data ) {
-                var x=JSON.stringify(data);
-                var parsedData = JSON.parse(x);
-                var cities = parsedData.def[0];
-                var city = JSON.stringify(cities);
+        //URL template for API to get info
+        var url1 = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=";
+        
+        //Retrieve word input from user
+        var url2 = document.forms["input"]["wordTyped"].value;
+        var url = url1+url2;
 
-
-                alert (city);
-            //   document.getElementById("button").innerHTML = JSON.stringify(data);
-
-            });
-
-            alert (range.toString ());
-     } 
+        //Call API to retrieve info and alert user
+        $.getJSON(url, function( data ) {
+            var x=JSON.stringify(data);
+            var parsedData = JSON.parse(x);
+            var cities = parsedData.def[0];
+            var city = JSON.stringify(cities);
+            alert (city);
+        });
     });
-});
+}) 
