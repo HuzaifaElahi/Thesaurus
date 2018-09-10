afc8da17d2f2debbf497d0f821160dcc35c38024
Huzaifa Elahi <huzaifaelahi1@yahoo.com>
2018-09-09 23:34:26 -0400

    Displayed API info

--- popup.js
+++ popup.js
@@ -17,13 +17,19 @@ chrome.browserAction.onClicked.addListener(function(){
         if (window.getSelection) {  
             var range = window.getSelection ();
 
-            var url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=";
-            var word = "laid";
-            var total = url + word;
-            $.getJSON(total, function( data ) {
-                x=data;
-                print(x);
-                alert (x);
+            var url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20180909T220155Z.87ac4f1b8d1a72e2.22505d83d95f0580847866bfefe1d0c7f5bd1087&lang=en-en&text=book";
+           // var word = "book";
+           // var total = url + word;
+            $.getJSON(url, function( data ) {
+                var x=JSON.stringify(data);
+                var parsedData = JSON.parse(x);
+                var cities = parsedData.def[0];
+                var city = JSON.stringify(cities);
+
+
+                alert (city);
+            //   document.getElementById("button").innerHTML = JSON.stringify(data);
+
             });
 
             alert (range.toString ());
